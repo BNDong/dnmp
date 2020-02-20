79676dca431d6ce1796cad702f717d49e04ffad2
BNDong <dbnuo@foxmail.com>
2020-02-20 09:55:12 +0800

    installExtensionFromTgz imagick

--- services/php/extensions/install.sh
+++ services/php/extensions/install.sh
@@ -324,8 +324,9 @@ if [[ -z "${EXTENSIONS##*,imagick,*}" ]]; then
     echo "---------- Install imagick ----------"
 	apk add --no-cache file-dev
 	apk add --no-cache imagemagick-dev
-    printf "\n" | pecl install imagick-3.4.4
-    docker-php-ext-enable imagick
+	installExtensionFromTgz imagick-3.4.4
+#    printf "\n" | pecl install imagick-3.4.4
+#    docker-php-ext-enable imagick
 fi
 
 if [[ -z "${EXTENSIONS##*,rar,*}" ]]; then
